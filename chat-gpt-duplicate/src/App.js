3742ed609b77c1c721a1f4b75f64cd0718361945
Demofont <artemios.arthur@gmail.com>
2023-09-26 00:23:14 +0200

    base chat gpt project is done

--- chat-gpt-duplicate/src/App.js
+++ chat-gpt-duplicate/src/App.js
@@ -1,27 +1,95 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+	const [ value, setValue] = useState(null)
+	const [ message, setMessage ] = useState(null)
+	const [ previousChats, setPreviousChats ] = useState([])
+	const [ currentTitle, setCurrentTitle ] = useState(null)
+	
+	const createNewChat = () => {
+		setMessage(null)
+		setValue("")
+		setCurrentTitle(null)
+	}
+	
+	const handleClick = (uniqueTitle) => {
+		setCurrentTitle(uniqueTitle)
+		setMessage(null)
+		setValue("")
+	}
+	
+	const getMessages = async () => {
+		const options = {
+			method: "POST",
+			body: JSON.stringify({
+				message: value
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+		try{
+			const response = await fetch('http://localhost:8000/completions', options)
+			const data = await response.json()
+			setMessage(data.choices[0].message)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	
+	useEffect(() => {
+		console.log(currentTitle, value, message)
+		if (!currentTitle && value && message){
+			setCurrentTitle(value)
+		}
+		if (currentTitle && value && message){
+			setPreviousChats(prevChats => (
+				[...prevChats,
+					{
+						title: currentTitle,
+						role: "user",
+						content: value
+					},
+					{
+						title: currentTitle,
+						role: message.role,
+						content:message.content
+					}
+				]
+			))
+		}
+	}, [message, currentTitle])
+	
+	console.log(previousChats)
+	
+	const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
+	const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+	
   return (
 	<div className="app">
 		<section className="side-bar">
-			<button>+ New chat</button>
+			<button onClick={createNewChat}>+ New chat</button>
 			<ul className="history">
-				<li>BLUGH</li>
+				{uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
 			</ul>
 			<nav>
 				<p>Made by Demofont</p>
 			</nav>
 		</section>
 		<section className="main">
-			<h1>DemofontGPT</h1>
-			<ul className="feed">
-			
+		{!currentTitle && <h1>DemofontGPT</h1>}
+			<ul className="feed">  
+				{currentChat.map((chatMessage, index) => <li key={index}>
+					<p className="role">{chatMessage.role}</p>
+					<p>{chatMessage.content}</p>
+				</li>)}
 			</ul>
 			
 			<div className="bottom-section">
 				<div className="intput-container">
-					<input/>
-					<div id="submit">➢</div>
+					<input value = {value} onChange={(e) => setValue(e.target.value)}/>
+					<div id="submit" onClick={getMessages}>➢</div>
 				</div>
 				<p className="info">
 					Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT August 3 Version
@@ -29,7 +97,7 @@ function App() {
 			</div>
 		</section>
 	</div>
-	)		
+	)
 }
 
 export default App;
